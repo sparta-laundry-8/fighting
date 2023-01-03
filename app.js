87f184a34908f898364cd1ba8be001d4d8996c03
handongjoo <dongjooyoung@naver.com>
2023-01-03 16:28:45 +0900

    프로필 수정 성공

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const loginRouter = require("./routes/login.js")
 const signupRouter = require("./routes/signup.js")
+const profileRouter = require("./routes/profile.js")
 
 app.use(express.json());
-app.use('/api',[loginRouter, signupRouter])
+app.use('/api',[loginRouter, signupRouter, profileRouter])
 
 app.listen(3000, () => {
     console.log("서버가 요청을 받을 준비가 됐어요");
